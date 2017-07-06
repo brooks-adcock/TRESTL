a8688d6a85794ab218dcd46336b05eda7ce83892
Brooks Adcock <bhi@brooksadcock.com>
2017-07-06 14:11:37 -0400

    Initial Commit

---
+++ TRESTL/TRESTL.h
@@ -0,0 +1,19 @@
+//
+//  TRESTL.h
+//  TRESTL
+//
+//  Created by Brooks Adcock on 7/6/17.
+//  Copyright © 2017 Brooks Adcock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRESTL.
+FOUNDATION_EXPORT double TRESTLVersionNumber;
+
+//! Project version string for TRESTL.
+FOUNDATION_EXPORT const unsigned char TRESTLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRESTL/PublicHeader.h>
+
+
